2635d46b49ba227ea7a847c03ba3f5756c5c7381
DBozhkovV <73282224+DBozhkovV@users.noreply.github.com>
2022-04-30 00:33:57 +0300

    Add files via upload

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,29 @@
 import React from 'react'
 import { Navbar,Nav,NavDropdown,Container} from 'react-bootstrap';
 import FormLogin from './FormLogin.js';
+import FormSignUp from './FormSignUp.js';
 
 const NavbarComponent = () => {
   const [LogInShow, setLogInShow] = React.useState(false);
-  
+  const [SignUpShow, setSignUpShow] = React.useState(false);
+
   return (
     <Navbar bg="dark" variant="dark">
-        <Container>
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                </NavDropdown>
-                <Nav.Link className='navPos' onClick={() => setLogInShow(true)}>Login</Nav.Link>
-                  <FormLogin show = {LogInShow} onHide={() => setLogInShow(false)}/>
-            </Nav>
-            </Navbar.Collapse>
-        </Container>
+      <Container>
+        <Navbar.Brand className="companyName" href="#home">
+          <img
+            className="opelLogo"
+          />{' '} Italiano Dealership</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav"> 
+        <Nav className="justify-content-end" style={{ width: "100%" }}>
+          <Nav.Link onClick={() => setLogInShow(true)}>Login</Nav.Link>
+            <FormLogin show = {LogInShow} onHide={() => setLogInShow(false)}/>
+          <Nav.Link onClick={() => setSignUpShow(true)}>Sign Up</Nav.Link>
+            <FormSignUp show={SignUpShow} onHide={() => setSignUpShow(false)}/>
+        </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   )
 }
